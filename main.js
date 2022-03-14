12f422b8121056afc0ecf8f799909901289dbb3b
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-03-14 11:35:15 -0400

    final commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let paragraph = document.querySelector("p");
+let heading = document.querySelector("h1");
+let item13 = document.querySelector("#item-13");
+let item3 = document.querySelector("#item-3");
+let image = document.querySelector("img");
+let item16 = document.querySelector("#item-16");
+
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+paragraph.style.color = "lightblue"; 
+heading.style.height = "48px";
+item13.style.opacity = "0.5"
+item3.innerText = 'I say, "Hi!"';
+
+image.style.height = "300px";
+image.style.width = "35px";
+
+item16.classList.add("item");
+item16.innerText = "Won't get fooled again.";
